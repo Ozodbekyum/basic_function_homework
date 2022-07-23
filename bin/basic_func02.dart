5a3acfd2cd73e6ba79dbac23f980a57e37a88641
Ozodbekyum <hello7ozod@gamil.com>
2022-07-23 09:38:25 +0500

    basic02

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,4 +1,11 @@
 //  Create a function called func.
 
 //  Return the value "Hello World"
-void main() {}
+void main() {
+ String y=func();
+ print(y);
+}
+
+String func(){
+    return 'Hello Wolrd';
+}
