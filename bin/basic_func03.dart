53a5b180bbe8d37703220bd032e92fb1916c16fa
Ozodbekyum <hello7ozod@gamil.com>
2022-07-23 09:41:24 +0500

    basic03

--- bin/basic_func03.dart
+++ bin/basic_func03.dart
@@ -1,4 +1,11 @@
 // Create a function called func.
 
 // Return the value "codeschooluz"
-void main() {}
+void main() {
+ String t=func();
+ print(t);
+}
+
+String func(){
+return 'codeschooluz';
+}
